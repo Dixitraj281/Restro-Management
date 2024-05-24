d1cfd49298da3858f8603782aace996deec03ca7
Dixit Raj <dixitraj281@gmail.com>
2024-05-24 18:34:01 +0530

    Your commit message

--- Backend/database.js
+++ Backend/database.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 const mongoURL = "mongodb+srv://ayushmongo:user9876@cluster0.vbnfhte.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(mongoURL,{useUnifiedTopology:true , useNewUrlParser:true} );
+mongoose.connect(mongoURL );
 var connection=mongoose.connection
 connection.on('error',()=>{
           console.log("mongo db connection failed");
