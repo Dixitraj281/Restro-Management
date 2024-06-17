32431a5baa3a829a3ff3fe7fcc9bf2013a01201d
Ayush Solanki <95464505+ayushdhatri@users.noreply.github.com>
2024-06-17 15:49:50 +0530

    Update database.js

--- Backend/database.js
+++ Backend/database.js
@@ -1,6 +1,5 @@
 const mongoose=require("mongoose");
-
-const mongoURL = "mongodb://localhost:27017/RestaurantManagementSystem";
+const mongoURL = "mongodb+srv://aayushmongo:user9876@cluster0.vbnfhte.mongodb.net/RestaurantManagementSystem";
 mongoose.connect(mongoURL);
 var connection=mongoose.connection
 connection.on('error',()=>{
